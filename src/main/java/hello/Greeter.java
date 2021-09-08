320ee5582ef7d927dcbc9f82bb20a15d65954377
Artem Kislovskiy <artem.kislovskiy@gmail.com>
2021-09-08 22:32:07 +0200

    Add basic CLI (#3)

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,13 @@
 package hello;
 
 public class Greeter {
-    public String sayHello() {
-        return "Hello world!";
+    public String sayHello(String[] args) {
+        String greeting;
+        if(args.length == 0) {
+            greeting = "Hello world!";
+        } else {
+            greeting = "The first argument is: " + args[0];
+        }
+        return greeting;
     }
 }
--- src/test/java/hello/HelloWorldTest.java
+++ src/test/java/hello/HelloWorldTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNotNull;
 public class HelloWorldTest {
     @Test public void appHasAGreeting() {
         Greeter greeter = new Greeter();
-        assertNotNull("app should have a greeting", greeter.sayHello());
+        String[] args = {"one", "two"};
+        assertNotNull("app should have a greeting", greeter.sayHello(args));
     }
 }
