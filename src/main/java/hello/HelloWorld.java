b17e2a7e8060a1ac77bc75a6a7787bdffb85a12e
Artem Kislovskiy <artem.kislovskiy@gmail.com>
2021-09-09 15:38:20 +0200

    Add better parsing with args4j (#5)

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,12 +2,37 @@ package hello;
 
 import org.joda.time.LocalTime;
 
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] argv) {
+        final Args args = new Args();
+        CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+        try {
+            parser.parseArgument(argv);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+            System.exit(-1);
+        }
+
         LocalTime currentTime = new LocalTime();
         System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello(args));
+        System.out.println(greeter.sayHello(args.inputString));
+    }
+
+    private static final class Args {
+        @Option(name = "-i",
+            aliases = "--input",
+            required = false,
+            usage = "a String to print to systemOut"
+        )
+        String inputString;
     }
 }
--- src/test/java/hello/HelloWorldTest.java
+++ src/test/java/hello/HelloWorldTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertNotNull;
 public class HelloWorldTest {
     @Test public void appHasAGreeting() {
         Greeter greeter = new Greeter();
-        String[] args = {"one", "two"};
-        assertNotNull("app should have a greeting", greeter.sayHello(args));
+        String testString = "Input";
+        assertNotNull("app should have a greeting", greeter.sayHello(testString));
     }
 }
